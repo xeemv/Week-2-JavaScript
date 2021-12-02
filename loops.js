3d079570630b32d4b18f06e5449bb24727e93330
xeemv <xeeyang08@gmail.com>
2021-12-02 12:36:28 -0600

    files

---
+++ loops.js
@@ -0,0 +1,40 @@
+//wk 2 teaching videos
+var x = 45;
+var y = 16;
+console.log( x - y);0
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log('whatever is next');
+
+//this will pull the even numbers out of the loop and only display those using the % sign
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+    console.log(i);
+    }
+}
+//while loop
+let names = ['Sam', 'Tom', 'Hillary'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+let a = 110;
+while (a < 10) {
+    console.log(a);
+    a++;
+}
+//do-while loop
+do {
+    console.log(a);
+} while (a < 10)
+//short version of writing the traditional four loop
+//this will repeat the name
+for (i in names) {
+    console.log(names[i]); //console.log(i);
+}
+//could use names instead of i
+//of will loop through the object/value of "i" and not the index
+//the difference of the names
+for (i of names) { //for (names of names) {
+    console.log(i); //console.log(names)}
+}
