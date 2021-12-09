f990bb6bb06646087794f445293fb5c008be10e8
xeemv <xeeyang08@gmail.com>
2021-12-08 19:36:30 -0600

    update

--- class.js
+++ class.js
@@ -2,9 +2,12 @@
 
 
 // ask user for their question
-// you need to create a (variable =) in order to save the response
+// var is out of date, the new use is let which is the same as var
 // prompt("What is your question?"); - starting code
-var question = prompt("What is your question?");
+var question = prompt("What is your question?"); // you need to create a (variable =) in order to save the response
+// Tip from Natalie - only write as much code as you can debug/get working
+// check the line of code on 7 to see if it work
+console.log(question); //just checked by typing in "hi" and it was saved
 
 
 //check the question for a few things
