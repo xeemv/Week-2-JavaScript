2cdb9e9f883a6d6887c46cb872f4f26a3e7df564
xeemv <xeeyang08@gmail.com>
2021-12-08 19:28:47 -0600

    creating a game w/ Natalie

---
+++ class.js
@@ -0,0 +1,12 @@
+/// creating a magic eight ball game w/ Natalie (instructor in class)
+
+
+// ask user for their question
+// you need to create a (variable =) in order to save the response
+// prompt("What is your question?"); - starting code
+var question = prompt("What is your question?");
+
+
+//check the question for a few things
+// give them an answer
+
